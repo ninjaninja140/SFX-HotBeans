6d0f866a26539f8bdd844af335235db48d7f1d09
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2024-11-08 12:48:31 +0000

    make sure to merge

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,21 @@
+import '@css/brand-slides.css';
+
+function importAll(r: __WebpackModuleApi.RequireContext): string[] {
+	return Array.from(new Map((r.keys().map(r) as string[]).map((path) => [path.split('/').pop(), path])).values());
+}
+
+const images: string[] = importAll(require.context('@content/Banners', false, /\.(png|svg)$/));
+
 export default () => {
+	console.log(images);
 	return (
 		<div
 			style={{
 				width: '70%',
-				height: '18px',
-				paddingTop: '3%',
-				paddingBottom: '3%',
+				height: '35px',
+				paddingTop: '2%',
+				paddingBottom: '2%',
 				backgroundColor: '#5865f2',
-				display: 'flex',
 				margin: '0 auto',
 				left: '0',
 				right: '0',
@@ -15,8 +23,33 @@ export default () => {
 				alignItems: 'center',
 				color: 'white',
 				borderRadius: '8px',
+				overflow: 'hidden',
+				position: 'relative',
 			}}>
-			Hi
+			<div
+				style={{
+					height: '100%',
+					width: '100%',
+					overflow: 'hidden',
+					whiteSpace: 'nowrap',
+					animation: 'bannermove 60s linear infinite',
+					position: 'absolute',
+					display: 'flex',
+				}}>
+				{images.map((src: string, key: number) => (
+					<img
+						src={src}
+						key={key}
+						style={{
+							height: '100%',
+							flex: '1',
+							width: '100%',
+							paddingLeft: '5%',
+							paddingRight: '5%',
+						}}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
