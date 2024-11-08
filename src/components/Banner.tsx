9a74be177e3bb75fc1a079c7afa53538a7be1832
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2024-11-08 10:01:26 +0000

    please for the love of god merge this when you get home

---
+++ src/components/Banner.tsx
@@ -0,0 +1,23 @@
+export default () => {
+	return (
+		<div
+			style={{
+				width: '70%',
+				height: '18px',
+				paddingTop: '3%',
+				paddingBottom: '3%',
+				backgroundColor: '#5865f2',
+				display: 'flex',
+				margin: '0 auto',
+				left: '0',
+				right: '0',
+				justifyContent: 'space-between',
+				alignItems: 'center',
+				color: 'white',
+				borderRadius: '8px',
+			}}>
+			Hi
+		</div>
+	);
+};
+
