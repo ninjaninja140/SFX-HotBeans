0e35e6941f647d868610b8369d026431163a318d
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2024-12-01 16:10:48 +0000

    Update imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import '@css/application.css';
-import Router from '@src/Router';
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import Router from './Router';
 
-import Footer from '@components/Footer';
-import Header from '@components/Header';
-import Head from '@components/Meta/Head';
-import '@css/application.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Head from './components/Meta/Head';
+import './css/application.css';
 
 const Application = () => {
 	const [isScrolled, setIsScrolled] = useState<boolean>(false);
